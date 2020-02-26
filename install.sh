959b8ae77cf7faa8e622b40988c2e28ea6b6532f
stefano maugeri <stefano@debian.stefano.legion>
2020-02-26 13:46:25 +0100

    installer

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
-sudo cp linked_list.c /usr/include/
+if [ ! -f /usr/include/linked_list.c ]; then
+    sudo cp linked_list.c /usr/include/
+fi
+
 gcc Example1/first_example.c -o first_example
 gcc Example2/second_example.c -o second_example
